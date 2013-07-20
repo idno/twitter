4d410d5c4227cb1be7dbe2d9a897d9c245d5f1fb
Ben Werdmuller <ben@benwerd.com>
2013-07-20 16:38:06 -0700

    Better trimming, and photo support.

--- Twitter/Main.php
+++ Twitter/Main.php
@@ -26,12 +26,42 @@
                         $twitterAPI = $this->connect();
                         $status_full =  $object->getDescription();
                         $status =       strip_tags($status_full);
+
+                        // Get links at this stage
+                        preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$status_full,$matches);
+
+                        global $url_matches; // ugh
+                        preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$status,$url_matches);
+
+                        $count_status = preg_replace('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i', '12345678901234567890123', $status);
+
+                        if (strlen($count_status) > 140) {
+                            $count_status = substr($count_status,0,115);
+                            if ($count_status[strlen($count_status) - 1] != ' ') {
+                                $count_status = substr($count_status,0,strrpos($count_status,' '));
+                            }
+                            $count_status = preg_replace_callback('/12345678901234567890123/',function($callback) {
+                                global $status_update_url_num;  // Ugh
+                                global $url_matches;			// Ugh ugh
+                                if (empty($status_update_url_num)) {
+                                    $status_update_url_num = 0;
+                                }
+                                if (!empty($url_matches[0][$status_update_url_num])) {
+                                    return $url_matches[0][$status_update_url_num];
+                                }
+                                $status_update_url_num++;
+                                return '';
+                            }, $count_status);
+                            $count_status .= ' .. ' . $object->getURL();
+                            $status = $count_status;
+                        }
+
                         $params = array(
                             'status' => $status
                         );
 
                         // Find any Twitter status IDs in case we need to mark this as a reply to them
-                        if (preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$status_full,$matches)) {
+                        if (!empty($matches[0])) {
                             foreach($matches[0] as $match) {
                                 if (parse_url($match, PHP_URL_HOST) == 'twitter.com') {
                                     preg_match('/[0-9]+/', $match, $status_matches);
@@ -66,7 +96,66 @@
                             'status' => $status
                         );
 
-                        $twitterAPI->request('POST', $twitterAPI->url('1.1/statuses/update'), $params);
+                        $response = $twitterAPI->request('POST', $twitterAPI->url('1.1/statuses/update'), $params);
+
+                        if (!empty($twitterAPI->response['response'])) {
+                            if ($json = json_decode($twitterAPI->response['response'])) {
+                                if (!empty($json->id_str)) {
+                                    $object->setPosseLink('twitter','https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
+                                    $object->save();
+                                }
+                            }
+                        }
+
+                    }
+                });
+
+                // Push "images" to Twitter
+                \Idno\Core\site()->addEventHook('post/image',function(\Idno\Core\Event $event) {
+                    if ($this->hasTwitter()) {
+                        $object = $event->data()['object'];
+                        $twitterAPI = $this->connect();
+                        $status = $object->getTitle();
+                        if (strlen($status) > 110) {                // Trim status down if required
+                            $status = substr($status, 0, 106) . ' ...';
+                        }
+
+                        $media = '';
+                        if ($attachments = $object->getAttachments()) {
+                            foreach($attachments as $attachment) {
+                                if ($bytes = \Idno\Entities\File::getFileDataByID((string) $attachment['_id'])) {
+                                    $media = '';
+                                    $filename = tempnam(sys_get_temp_dir(), 'idnotwitter');
+                                    file_put_contents($filename, $bytes);
+                                    $media .= "@{$filename};type=" . $attachment['mime_type'] . ';filename=' . $attachment['filename'];
+                                }
+                            }
+                        }
+
+                        $params = array(
+                            'status' => $status,
+                            'media[]' => $media
+                        );
+
+                        $response = $twitterAPI->request('POST', $twitterAPI->url('1.1/statuses/update_with_media'), $params, true, true);
+                        /*$code = $twitterAPI->request( 'POST','https://upload.twitter.com/1.1/statuses/update_with_media',
+                            $params,
+                            true, // use auth
+                            true  // multipart
+                        );*/
+
+                        @unlink($filename);
+
+                        error_log(var_export($twitterAPI->response['response'],true));
+                        if (!empty($twitterAPI->response['response'])) {
+                            if ($json = json_decode($twitterAPI->response['response'])) {
+                                if (!empty($json->id_str)) {
+                                    $object->setPosseLink('twitter','https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
+                                    $object->save();
+                                }
+                            }
+                        }
+
                     }
                 });
             }
