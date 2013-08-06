2446cd8942923870ed70c5e67b5e6cae9b517148
Ben Werdmuller <ben@benwerd.com>
2013-08-05 18:37:29 -0700

    Code cleanup; fixing error where long tweets may cause API errors.

--- Twitter/Main.php
+++ Twitter/Main.php
@@ -2,47 +2,50 @@
 
     namespace IdnoPlugins\Twitter {
 
-        class Main extends \Idno\Common\Plugin {
+        class Main extends \Idno\Common\Plugin
+        {
 
-            function registerPages() {
+            function registerPages()
+            {
                 // Register the callback URL
-                    \Idno\Core\site()->addPageHandler('twitter/callback','\IdnoPlugins\Twitter\Pages\Callback');
+                \Idno\Core\site()->addPageHandler('twitter/callback', '\IdnoPlugins\Twitter\Pages\Callback');
                 // Register admin settings
-                    \Idno\Core\site()->addPageHandler('admin/twitter','\IdnoPlugins\Twitter\Pages\Admin');
+                \Idno\Core\site()->addPageHandler('admin/twitter', '\IdnoPlugins\Twitter\Pages\Admin');
                 // Register settings page
-                    \Idno\Core\site()->addPageHandler('account/twitter','\IdnoPlugins\Twitter\Pages\Account');
+                \Idno\Core\site()->addPageHandler('account/twitter', '\IdnoPlugins\Twitter\Pages\Account');
 
                 /** Template extensions */
                 // Add menu items to account & administration screens
-                    \Idno\Core\site()->template()->extendTemplate('admin/menu/items','admin/twitter/menu');
-                    \Idno\Core\site()->template()->extendTemplate('account/menu/items','account/twitter/menu');
+                \Idno\Core\site()->template()->extendTemplate('admin/menu/items', 'admin/twitter/menu');
+                \Idno\Core\site()->template()->extendTemplate('account/menu/items', 'account/twitter/menu');
             }
 
-            function registerEventHooks() {
+            function registerEventHooks()
+            {
                 // Push "notes" to Twitter
-                \Idno\Core\site()->addEventHook('post/note',function(\Idno\Core\Event $event) {
+                \Idno\Core\site()->addEventHook('post/note', function (\Idno\Core\Event $event) {
                     if ($this->hasTwitter()) {
-                        $object = $event->data()['object'];
-                        $twitterAPI = $this->connect();
-                        $status_full =  $object->getDescription();
-                        $status =       strip_tags($status_full);
+                        $object      = $event->data()['object'];
+                        $twitterAPI  = $this->connect();
+                        $status_full = $object->getDescription();
+                        $status      = strip_tags($status_full);
 
                         // Get links at this stage
-                        preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$status_full,$matches);
+                        preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i', $status_full, $matches);
 
                         global $url_matches; // ugh
-                        preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i',$status,$url_matches);
+                        preg_match_all('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i', $status, $url_matches);
 
                         $count_status = preg_replace('/((ht|f)tps?:\/\/[^\s\r\n\t<>"\'\(\)]+)/i', '12345678901234567890123', $status);
 
                         if (strlen($count_status) > 140) {
-                            $count_status = substr($count_status,0,115);
+                            $count_status = substr($count_status, 0, 115);
                             if ($count_status[strlen($count_status) - 1] != ' ') {
-                                $count_status = substr($count_status,0,strrpos($count_status,' '));
+                                $count_status = substr($count_status, 0, strrpos($count_status, ' '));
                             }
-                            $count_status = preg_replace_callback('/12345678901234567890123/',function($callback) {
-                                global $status_update_url_num;  // Ugh
-                                global $url_matches;			// Ugh ugh
+                            $count_status = preg_replace_callback('/12345678901234567890123/', function ($callback) {
+                                global $status_update_url_num; // Ugh
+                                global $url_matches; // Ugh ugh
                                 if (empty($status_update_url_num)) {
                                     $status_update_url_num = 0;
                                 }
@@ -50,11 +53,14 @@
                                     return $url_matches[0][$status_update_url_num];
                                 }
                                 $status_update_url_num++;
+
                                 return '';
                             }, $count_status);
                             $count_status .= ' .. ' . $object->getURL();
                             $status = $count_status;
                         }
+
+                        $status = preg_replace('/[ ]{2,}/',' ',$status);
 
                         $params = array(
                             'status' => $status
@@ -62,7 +68,7 @@
 
                         // Find any Twitter status IDs in case we need to mark this as a reply to them
                         if (!empty($matches[0])) {
-                            foreach($matches[0] as $match) {
+                            foreach ($matches[0] as $match) {
                                 if (parse_url($match, PHP_URL_HOST) == 'twitter.com') {
                                     preg_match('/[0-9]+/', $match, $status_matches);
                                     $params['in_reply_to_status_id'] = $status_matches[0];
@@ -71,24 +77,25 @@
                         }
 
                         $response = $twitterAPI->request('POST', $twitterAPI->url('1.1/statuses/update'), $params);
-    					if (!empty($twitterAPI->response['response'])) {
-							if ($json = json_decode($twitterAPI->response['response'])) {
-								if (!empty($json->id_str)) {
-									$object->setPosseLink('twitter','https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
-									$object->save();
-								}
-							}
-						}
+                        error_log(var_export($response, true));
+                        if (!empty($twitterAPI->response['response'])) {
+                            if ($json = json_decode($twitterAPI->response['response'])) {
+                                if (!empty($json->id_str)) {
+                                    $object->setPosseLink('twitter', 'https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
+                                    $object->save();
+                                }
+                            }
+                        }
                     }
                 });
 
                 // Push "articles" to Twitter
-                \Idno\Core\site()->addEventHook('post/article',function(\Idno\Core\Event $event) {
+                \Idno\Core\site()->addEventHook('post/article', function (\Idno\Core\Event $event) {
                     if ($this->hasTwitter()) {
-                        $object = $event->data()['object'];
+                        $object     = $event->data()['object'];
                         $twitterAPI = $this->connect();
-                        $status = $object->getTitle();
-                        if (strlen($status) > 110) {                // Trim status down if required
+                        $status     = $object->getTitle();
+                        if (strlen($status) > 110) { // Trim status down if required
                             $status = substr($status, 0, 106) . ' ...';
                         }
                         $status .= ' ' . $object->getURL();
@@ -101,7 +108,7 @@
                         if (!empty($twitterAPI->response['response'])) {
                             if ($json = json_decode($twitterAPI->response['response'])) {
                                 if (!empty($json->id_str)) {
-                                    $object->setPosseLink('twitter','https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
+                                    $object->setPosseLink('twitter', 'https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
                                     $object->save();
                                 }
                             }
@@ -111,18 +118,18 @@
                 });
 
                 // Push "images" to Twitter
-                \Idno\Core\site()->addEventHook('post/image',function(\Idno\Core\Event $event) {
+                \Idno\Core\site()->addEventHook('post/image', function (\Idno\Core\Event $event) {
                     if ($this->hasTwitter()) {
-                        $object = $event->data()['object'];
+                        $object     = $event->data()['object'];
                         $twitterAPI = $this->connect();
-                        $status = $object->getTitle();
-                        if (strlen($status) > 110) {                // Trim status down if required
+                        $status     = $object->getTitle();
+                        if (strlen($status) > 110) { // Trim status down if required
                             $status = substr($status, 0, 106) . ' ...';
                         }
 
                         // Let's first try getting the thumbnail
                         if (!empty($object->thumbnail_id)) {
-                            if ($thumb = (array) \Idno\Entities\File::getByID($object->thumbnail_id)) {
+                            if ($thumb = (array)\Idno\Entities\File::getByID($object->thumbnail_id)) {
                                 $attachments = array($thumb['file']);
                             }
                         }
@@ -133,9 +140,9 @@
                         }
 
                         if (!empty($attachments)) {
-                            foreach($attachments as $attachment) {
-                                if ($bytes = \Idno\Entities\File::getFileDataByID((string) $attachment['_id'])) {
-                                    $media = '';
+                            foreach ($attachments as $attachment) {
+                                if ($bytes = \Idno\Entities\File::getFileDataByID((string)$attachment['_id'])) {
+                                    $media    = '';
                                     $filename = tempnam(sys_get_temp_dir(), 'idnotwitter');
                                     file_put_contents($filename, $bytes);
                                     $media .= "@{$filename};type=" . $attachment['mime_type'] . ';filename=' . $attachment['filename'];
@@ -144,7 +151,7 @@
                         }
 
                         $params = array(
-                            'status' => $status,
+                            'status'  => $status,
                             'media[]' => $media
                         );
 
@@ -157,11 +164,11 @@
 
                         @unlink($filename);
 
-                        error_log(var_export($twitterAPI->response['response'],true));
+                        error_log(var_export($twitterAPI->response['response'], true));
                         if (!empty($twitterAPI->response['response'])) {
                             if ($json = json_decode($twitterAPI->response['response'])) {
                                 if (!empty($json->id_str)) {
-                                    $object->setPosseLink('twitter','https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
+                                    $object->setPosseLink('twitter', 'https://twitter.com/' . $json->user->screen_name . '/status/' . $json->id_str);
                                     $object->save();
                                 }
                             }
@@ -177,7 +184,8 @@
              *
              * @return bool|\tmhOAuth
              */
-            function connect() {
+            function connect()
+            {
                 require_once(dirname(__FILE__) . '/external/tmhOAuth/tmhOAuth.php');
                 require_once(dirname(__FILE__) . '/external/tmhOAuth/tmhUtilities.php');
                 if (!empty(\Idno\Core\site()->config()->twitter)) {
@@ -188,8 +196,10 @@
                     if (!empty(\Idno\Core\site()->session()->currentUser()->twitter)) {
                         $params = array_merge($params, \Idno\Core\site()->session()->currentUser()->twitter);
                     }
+
                     return new \tmhOAuth($params);
                 }
+
                 return false;
             }
 
@@ -197,10 +207,12 @@
              * Can the current user use Twitter?
              * @return bool
              */
-            function hasTwitter() {
+            function hasTwitter()
+            {
                 if (\Idno\Core\site()->session()->currentUser()->twitter) {
                     return true;
                 }
+
                 return false;
             }
 
