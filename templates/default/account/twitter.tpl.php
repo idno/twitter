e2f08b56492ed9ed9e9114dee46f918a99435b6b
Ben Werdmuller <ben@benwerd.com>
2015-01-03 19:05:42 -0800

    Improved account connection page

--- templates/default/account/twitter.tpl.php
+++ templates/default/account/twitter.tpl.php
@@ -9,7 +9,12 @@
 </div>
 <div class="row">
     <div class="span10 offset1">
-        <form action="/account/twitter/" class="form-horizontal" method="post">
+    <?php
+
+            if (!empty(\Idno\Core\site()->config()->twitter['consumer_key']) && !empty(\Idno\Core\site()->config()->twitter['consumer_secret'])) {
+
+                ?>
+        <form action="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/twitter/" class="form-horizontal" method="post">
             <?php
               if (empty(\Idno\Core\site()->session()->currentUser()->twitter)) {
             ?>
@@ -21,7 +26,7 @@
                         Easily share updates, posts, and pictures to Twitter. </p>
                         <p>
 	                        With Twitter connected, you can cross-post content that you publish publicly on your site. 
-                    </p>
+                    	</p>
 	                </div>
 	                </div>
                     
@@ -36,7 +41,7 @@
             </div>
             <?php
 
-              } else {
+				} else if (!\Idno\Core\site()->config()->multipleSyndicationAccounts()) {
 
             ?>
                   <div class="control-group">
@@ -61,9 +66,81 @@
 
             <?php
 
+              } else {
+              
+              ?>
+              		<div class="control-group">
+                      <div class="controls-config">
+	                    <div class="row">
+						<div class="span6">
+                          <p>
+							You have connected the following accounts to Twitter:
+                          </p>
+						</div>
+						</div>
+						<?php
+
+                                        if ($accounts = \Idno\Core\site()->syndication()->getServiceAccounts('twitter')) {
+
+                                            foreach ($accounts as $account) {
+
+                                                ?>
+                                                <p>
+                                                    <input type="hidden" name="remove" value="<?= $account['username'] ?>"/>
+                                                    <button type="submit"
+                                                            class="connect tw connected">@<?= $account['username'] ?></button>
+                                                </p>
+                                            <?php
+
+                                            }
+
+                                        }
+
+                                    ?>
+                          <p>
+                                        <a href="<?= $vars['oauth_url'] ?>" class="">Click here
+                                            to connect another Twitter account</a>
+                                    </p>
+                      </div>
+                  </div>
+              <?php
+              
               }
+              
             ?>
+            
             <?= \Idno\Core\site()->actions()->signForm('/account/twitter/')?>
+            
         </form>
+                    <?php
+
+            } else {
+
+                if (\Idno\Core\site()->session()->currentUser()->isAdmin()) {
+
+                    ?>
+                    <p>
+                        Before you can begin connecting to Twitter, you need to set it up.
+                    </p>
+                    <p>
+                        <a href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>admin/twitter/">Click here to begin
+                            Twitter configuration.</a>
+                    </p>
+                <?php
+
+                } else {
+
+                    ?>
+                    <p>
+                        The administrator has not finished setting up Twitter on this site.
+                        Please come back later.
+                    </p>
+                <?php
+
+                }
+
+            }
+
+        ?>
     </div>
 </div>
