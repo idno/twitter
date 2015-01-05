0ba775049444a8a0f4df788f146b2db42995d18a
Ben Werdmuller <ben@benwerd.com>
2015-01-04 19:06:23 -0800

    Trim input
    Fixes #14 ;)

--- Pages/Admin.php
+++ Pages/Admin.php
@@ -22,8 +22,8 @@
 
             function postContent() {
                 $this->adminGatekeeper(); // Admins only
-                $consumer_key = $this->getInput('consumer_key');
-                $consumer_secret = $this->getInput('consumer_secret');
+                $consumer_key = trim($this->getInput('consumer_key'));
+                $consumer_secret = trim($this->getInput('consumer_secret'));
                 \Idno\Core\site()->config->config['twitter'] = array(
                     'consumer_key' => $consumer_key,
                     'consumer_secret' => $consumer_secret
