9ef938f37dce430caac59013cc34c2808b0e382d
Kyle Mahan <kyle.mahan@gmail.com>
2016-03-16 18:10:54 -0700

    automatically add an @-name when replying
    - if it isn't already included
    - and isn't the current user (you don't have to include an @-name when
      replying to yourself)

--- Main.php
+++ Main.php
@@ -76,16 +76,37 @@
                 \Idno\Core\Idno::site()->addEventHook('post/note/twitter', function (\Idno\Core\Event $event) {
                     $eventdata = $event->data();
                     if ($this->hasTwitter()) {
-                        $object      = $eventdata['object'];
+                        $object = $eventdata['object'];
                         if (!empty($eventdata['syndication_account'])) {
-                            $twitterAPI  = $this->connect($eventdata['syndication_account']);
+                            $twitterAPI = $this->connect($eventdata['syndication_account']);
+                            $screenName = $eventdata['syndication_account'];
                         } else {
-                            $twitterAPI  = $this->connect();
+                            $twitterAPI = $this->connect();
+                            $screenName = isset(\Idno\Core\Idno::site()->session()->currentUser()->twitter['screen_name'])
+                                    ? \Idno\Core\Idno::site()->session()->currentUser()->twitter['screen_name']
+                                    : false;
                         }
+
+                        $params = [];
+
                         $status_full = trim($object->getDescription());
                         $status      = preg_replace('/<[^\>]*>/', '', $status_full); //strip_tags($status_full);
                         $status      = str_replace("\r", '', $status);
                         $status      = html_entity_decode($status);
+
+                        // Find any Twitter status IDs in case we need to mark this as a reply to them
+                        $inreplytourls = array_merge((array) $object->inreplyto, (array) $object->syndicatedto);
+                        if ($inreplyto = self::findTwitterStatus($inreplytourls)) {
+                            $params['in_reply_to_status_id'] = $inreplyto['status_id'];
+
+                            // if inreplytoname is not in the status, and is not this user's name, then prepend it to the status
+                            $replyName = $inreplyto['screen_name'];
+                            if ($replyName
+                                    && mb_strtolower($screenName) !== mb_strtolower($replyName)
+                                    && mb_stristr($status, '@'.$replyName) === false) {
+                                $status = '@' . $replyName . ' ' . $status;
+                            }
+                        }
 
                         // Permalink will be included if the status message is truncated
                         $permalink      = $object->getSyndicationURL();
@@ -95,15 +116,7 @@
 
                         \Idno\Core\Idno::site()->logging()->log("status after shortening: $status");
 
-                        $params = array(
-                            'status' => trim($status)
-                        );
-
-                        // Find any Twitter status IDs in case we need to mark this as a reply to them
-                        $inreplytourls = array_merge((array) $object->inreplyto, (array) $object->syndicatedto);
-                        if ($inreplytoid = self::findTwitterStatusId($inreplytourls)) {
-                            $params['in_reply_to_status_id'] = $inreplytoid;
-                        }
+                        $params['status'] = trim($status);
 
                         $response = $twitterAPI->request('POST', $twitterAPI->url('1.1/statuses/update'), $params);
                         if (!empty($twitterAPI->response['response'])) {
@@ -305,8 +318,8 @@
                         $params = array();
                         // Find the status ID of the tweet that was liked
                         $likeofurls = array_merge((array) $object->likeof, (array) $object->syndicatedto);
-                        if ($likeofid = self::findTwitterStatusId($likeofurls)) {
-                            $params['id'] = $likeofid;
+                        if ($likeof = self::findTwitterStatus($likeofurls)) {
+                            $params['id'] = $likeof['status_id'];
                         }
                         else {
                             \Idno\Core\Idno::site()->logging()->log("Could not find a status to like");
@@ -339,8 +352,8 @@
                         $params = array();
                         // Find the status ID of the tweet that was reposted
                         $repostofurls = array_merge((array) $object->repostof, (array) $object->syndicatedto);
-                        if ($repostofid = self::findTwitterStatusId($repostofurls)) {
-                            $params['id'] = $repostofid;
+                        if ($repostof = self::findTwitterStatus($repostofurls)) {
+                            $params['id'] = $repostof['status_id'];
                         } else {
                             \Idno\Core\Idno::site()->logging()->log("Could not find a status to retweet");
                             return;
@@ -367,16 +380,22 @@
 
             /**
              * Search a list of URLs for one that looks like a Tweet
-             * permalink and return its status id.
+             * permalink and return an array with the Tweet's
+             * 'status_id' and 'screen_name'.
              * @param array urls
-             * @return string or false
+             * @return array or false
              */
-            private static function findTwitterStatusId($urls)
+            private static function findTwitterStatus($urls)
             {
                 foreach ($urls as $url) {
-                    if (parse_url($url, PHP_URL_HOST) == 'twitter.com') {
-                        preg_match('/[0-9]{8,}/', $url, $status_matches);
-                        return $status_matches[0];
+                    if (preg_match('/(www\.|m\.)?twitter.com/i', parse_url($url, PHP_URL_HOST))) {
+                        $path = explode('/', parse_url($url, PHP_URL_PATH));
+                        if (count($path) >= 4) {
+                            return [
+                                'screen_name' => $path[1],
+                                'status_id'   => $path[3],
+                            ];
+                        }
                     }
                 }
                 return false;
