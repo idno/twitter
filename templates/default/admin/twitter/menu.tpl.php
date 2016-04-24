6ba612932abfea712ae7699bfbb341615e519794
Kyle Mahan <kyle.mahan@gmail.com>
2016-04-24 08:15:28 -0700

    update Account link to use full path
    for the benefit of subdirectory installations.
    
    fixes #45

--- templates/default/admin/twitter/menu.tpl.php
+++ templates/default/admin/twitter/menu.tpl.php
@@ -1,1 +1,1 @@
-<li <?php if ($_SERVER['REQUEST_URI'] == '/admin/twitter/') echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/twitter/">Twitter</a></li>
+<li <?php if (strpos($_SERVER['REQUEST_URI'], '/admin/twitter/') !== false) echo 'class="active"'; ?>><a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/twitter/">Twitter</a></li>
